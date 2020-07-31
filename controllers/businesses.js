c6efdd6c18f8883918c5a6c067b75ac59250f28a
Joshua Knight <Joshua_Knight@alumni.brown.edu>
2020-07-31 13:08:35 -0500

    feat: added async handler

--- controllers/businesses.js
+++ controllers/businesses.js
@@ -1,87 +1,68 @@
 const Business = require('../models/Business')
 const ErrorResponse = require('../utils/errorResponse')
+const asyncHandler = require('../middleware/async')
 
 //@desc Get all businesses
 //@route GET /api/v1/businesses
 //@access Public
-exports.getBusinesses = async (req, res, next) => {
-  try {
-    const businesses = await Business.find()
+exports.getBusinesses = asyncHandler(async (req, res, next) => {
+  const businesses = await Business.find()
 
-    res
-      .status(200)
-      .json({ success: true, count: businesses.length, data: businesses })
-  } catch (err) {
-    next(err)
-  }
-}
+  res
+    .status(200)
+    .json({ success: true, count: businesses.length, data: businesses })
+})
 
 //@desc Get a single businesss
 //@route GET /api/v1/businesses/:id
 //@access Public
-exports.getBusiness = async (req, res, next) => {
-  try {
-    const business = await Business.findById(req.params.id)
+exports.getBusiness = asyncHandler(async (req, res, next) => {
+  const business = await Business.findById(req.params.id)
 
-    if (!business) {
-      return next(err)
-    }
-
-    res.status(200).json({ success: true, data: business })
-  } catch (err) {
-    next(err)
+  if (!business) {
+    return next(err)
   }
-}
+
+  res.status(200).json({ success: true, data: business })
+})
 
 //@desc Create a new businesss
 //@route GET /api/v1/businesses
 //@access Private
-exports.createBusiness = async (req, res, next) => {
-  try {
-    const business = await Business.create(req.body)
+exports.createBusiness = asyncHandler(async (req, res, next) => {
+  const business = await Business.create(req.body)
 
-    res.status(201).json({
-      success: true,
-      data: business,
-    })
-  } catch (err) {
-    next(err)
-  }
-}
+  res.status(201).json({
+    success: true,
+    data: business,
+  })
+})
 
 //@desc Update a business
 //@route PUT /api/v1/businesses/:id
 //@access Private
-exports.updateBusiness = async (req, res, next) => {
-  try {
-    const business = await Business.findByIdAndUpdate(req.params.id, req.body, {
-      new: true,
-      runValidators: true,
-    })
-    //make sure business exists
-    if (!business) {
-      next(err)
-    }
-
-    res.status(200).json({ success: true, data: business })
-  } catch (err) {
+exports.updateBusiness = asyncHandler(async (req, res, next) => {
+  const business = await Business.findByIdAndUpdate(req.params.id, req.body, {
+    new: true,
+    runValidators: true,
+  })
+  //make sure business exists
+  if (!business) {
     next(err)
   }
-}
+
+  res.status(200).json({ success: true, data: business })
+})
 
 //@desc Delete a business
 //@route PUT /api/v1/businesses/:id
 //@access Private
-exports.deleteBusiness = async (req, res, next) => {
-  try {
-    const business = await Business.findByIdAndDelete(req.params.id)
-    //make sure business exists
-    if (!business) {
-      next(err)
-    }
-
-    res.status(200).json({ success: true, data: 'business deleted' })
-  } catch (err) {
+exports.deleteBusiness = asyncHandler(async (req, res, next) => {
+  const business = await Business.findByIdAndDelete(req.params.id)
+  //make sure business exists
+  if (!business) {
     next(err)
   }
-}
+
+  res.status(200).json({ success: true, data: 'business deleted' })
+})
