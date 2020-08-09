dcdae9f1f6bd42e61138d7149e566e7c85a92acf
Joshua Knight <Joshua_Knight@alumni.brown.edu>
2020-08-08 19:19:54 -0500

    feat: added login route

--- controllers/auth.js
+++ controllers/auth.js
@@ -21,3 +21,31 @@ exports.register = asyncHandler(async (req, res, next) => {
 
   res.status(200).json({ success: true, token })
 })
+
+//@desc Login user
+//@route POST /api/v1/auth/login
+//@access Public
+exports.login = asyncHandler(async (req, res, next) => {
+  const { email, password } = req.body
+
+  //validate email and pw
+  if (!email || !password) {
+    return next(new ErrorResponse('Please provide an email and password', 400))
+  }
+
+  //Check user
+  const user = await User.findOne({ email }).select('+password')
+  //validate user with email
+  if (!user) {
+    return next(new ErrorResponse('Invalid credentials', 401))
+  }
+  //check if password matches..returns true or false based on if a match
+  const isMatch = await user.matchPassword(password)
+  if (!isMatch) {
+    return next(new ErrorResponse('Invalid credentials', 401))
+  }
+  //Create Token
+  const token = user.getSignedJwtToken()
+
+  res.status(200).json({ success: true, token })
+})
