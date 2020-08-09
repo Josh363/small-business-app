4d1f0e2f3bcd9da3fa4e9b9838ee55ddf4a65274
Joshua Knight <Joshua_Knight@alumni.brown.edu>
2020-08-08 19:36:04 -0500

    feat: send JWT in a cookie to client

--- controllers/auth.js
+++ controllers/auth.js
@@ -16,10 +16,7 @@ exports.register = asyncHandler(async (req, res, next) => {
     role,
   })
 
-  //Create Token
-  const token = user.getSignedJwtToken()
-
-  res.status(200).json({ success: true, token })
+  sendTokenResponse(user, 200, res)
 })
 
 //@desc Login user
@@ -44,8 +41,28 @@ exports.login = asyncHandler(async (req, res, next) => {
   if (!isMatch) {
     return next(new ErrorResponse('Invalid credentials', 401))
   }
-  //Create Token
-  const token = user.getSignedJwtToken()
-
-  res.status(200).json({ success: true, token })
+  //call sendtoken response
+  sendTokenResponse(user, 200, res)
 })
+
+//Get token from model, create cookie, send response
+const sendTokenResponse = (user, statusCode, res) => {
+  //create token
+  const token = user.getSignedJwtToken()
+  //cookie options
+  const options = {
+    expires: new Date(
+      Date.now() + process.env.JWT_COOKIE_EXPIRE * 24 * 60 * 60 * 1000
+    ),
+    httpOnly: true,
+  }
+  //secure cookie if in production
+  if (process.env.NODE_ENV === 'production') {
+    options.secure = true
+  }
+  //res object
+  res.status(statusCode).cookie('token', token, options).json({
+    success: true,
+    token,
+  })
+}
