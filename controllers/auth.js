3ba6b6c05e253331536608b60e97e230710605de
Joshua Knight <Joshua_Knight@alumni.brown.edu>
2020-08-10 01:45:07 -0500

    feat: added update user details/password routes

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,6 +1,8 @@
 const User = require('../models/User')
 const ErrorResponse = require('../utils/errorResponse')
 const asyncHandler = require('../middleware/async')
+const sendEmail = require('../utils/sendEmail')
+const crypto = require('crypto')
 
 //@desc Register user
 //@route Get /api/v1/auth/register
@@ -57,6 +59,45 @@ exports.getMe = asyncHandler(async (req, res, next) => {
   })
 })
 
+//@desc Update User details
+//@route POST /api/v1/auth/updatedetails
+//@access Private
+exports.updateDetails = asyncHandler(async (req, res, next) => {
+  //update fields object
+  const fieldsToUpdate = {
+    name: req.body.name,
+    email: req.body.email,
+  }
+
+  const user = await User.findByIdAndUpdate(req.user.id, fieldsToUpdate, {
+    new: true,
+    runValidators: true,
+  })
+
+  res.status(200).json({
+    success: true,
+    data: user,
+  })
+})
+
+//@desc Update Password
+//@route PUT /api/v1/auth/updatepassword
+//@access Private
+exports.updatePassword = asyncHandler(async (req, res, next) => {
+  //find user with password
+  const user = await User.findById(req.user.id).select('+password')
+  //check current password
+  const isMatch = await user.matchPassword(req.body.currentPassword)
+  if (!isMatch) {
+    return next(new ErrorResponse('Invalid credentials', 401))
+  }
+  //if isMatch is true
+  user.password = req.body.newPassword
+  await user.save()
+
+  sendTokenResponse(user, 200, res)
+})
+
 //@desc Forgot password
 //@route POST /api/v1/auth/forgotpassword
 //@access Public
@@ -72,10 +113,63 @@ exports.forgotPassword = asyncHandler(async (req, res, next) => {
   //save token to db..run validateBeforeSave to stop from validating name required, etc
   await user.save({ validateBeforeSave: false })
 
-  res.status(200).json({
-    success: true,
-    data: user,
+  // Create reset url
+  const resetUrl = `${req.protocol}://${req.get(
+    'host'
+  )}/api/v1/auth/resetpassword/${resetToken}`
+
+  const message = `You are receiving this email because you requested a password reset. 
+  Please make a PUT request to : \n \n ${resetUrl}`
+
+  console.log(resetUrl)
+
+  //call sendemail
+  try {
+    await sendEmail({
+      email: user.email,
+      subject: 'Password reset token',
+      message,
+    })
+
+    res.status(200).json({ success: true, data: 'Email sent' })
+  } catch (err) {
+    console.log(err)
+    user.resetPasswordToken = undefined
+    user.resetPasswordExpire = undefined
+
+    await user.save({ validateBeforeSave: false })
+
+    return next(new ErrorResponse('Email could not be sent', 500))
+  }
+})
+
+//@desc Reset password
+//@route PUT /api/v1/auth/resetPassword/:resettoken
+//@access Public
+exports.resetPassword = asyncHandler(async (req, res, next) => {
+  //get hashed token
+  const resetPasswordToken = crypto
+    .createHash('sha256')
+    .update(req.params.resettoken)
+    .digest('hex')
+  //find user by token and check expiration date
+  const user = await User.findOne({
+    resetPasswordToken,
+    //make sure expiration date is greater than today
+    resetPasswordExpire: { $gt: Date.now() },
   })
+  //if user validation fails..
+  if (!user) {
+    return next(new ErrorResponse('Invalid token', 400))
+  }
+
+  //set new password if validation passes
+  user.password = req.body.password
+  user.resetPasswordToken = undefined
+  user.resetPasswordExpire = undefined
+  await user.save()
+
+  sendTokenResponse(user, 200, res)
 })
 
 //Get token from model, create cookie, send response
--- routes/auth.js
+++ routes/auth.js
@@ -4,6 +4,9 @@ const {
   login,
   getMe,
   forgotPassword,
+  resetPassword,
+  updateDetails,
+  updatePassword,
 } = require('../controllers/auth')
 
 const router = express.Router()
@@ -16,6 +19,12 @@ router.post('/login', login)
 
 router.get('/me', protect, getMe)
 
+router.put('/updatedetails', protect, updateDetails)
+
+router.put('/updatepassword', protect, updatePassword)
+
 router.post('/forgotpassword', forgotPassword)
+
+router.put('/resetpassword/:resettoken', resetPassword)
 
 module.exports = router
