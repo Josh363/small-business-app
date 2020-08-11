138d836e7cf79619b10926856755fde98b4e0322
Joshua Knight <Joshua_Knight@alumni.brown.edu>
2020-08-11 13:06:31 -0500

    feat: update and deleteReview routes created

--- controllers/reviews.js
+++ controllers/reviews.js
@@ -76,3 +76,52 @@ exports.addReview = asyncHandler(async (req, res, next) => {
     data: review,
   })
 })
+
+//@desc Update review
+//@route PUT /api/v1/reviews/:id
+//@access Private
+exports.updateReview = asyncHandler(async (req, res, next) => {
+  //check if business exists
+  let review = await Review.findById(req.params.id)
+
+  if (!review) {
+    next(new ErrorResponse(`Review ${req.params.id} does not exist`, 404))
+  }
+  //check to see if review belongs to user or user is admin
+  if (review.user.toString() !== req.user.id && req.user.role !== 'admin') {
+    return next(new ErrorResponse(`User ${req.user.id} is not authorized`, 401))
+  }
+
+  review = await Review.findByIdAndUpdate(req.params.id, req.body, {
+    new: true,
+    runValidators: true,
+  })
+
+  res.status(200).json({
+    success: true,
+    data: review,
+  })
+})
+
+//@desc Delete review
+//@route DELETE /api/v1/reviews/:id
+//@access Private
+exports.deleteReview = asyncHandler(async (req, res, next) => {
+  //check if business exists
+  let review = await Review.findById(req.params.id)
+
+  if (!review) {
+    next(new ErrorResponse(`Review ${req.params.id} does not exist`, 404))
+  }
+  //check to see if review belongs to user or user is admin
+  if (review.user.toString() !== req.user.id && req.user.role !== 'admin') {
+    return next(new ErrorResponse(`User ${req.user.id} is not authorized`, 401))
+  }
+
+  await review.remove()
+
+  res.status(200).json({
+    success: true,
+    data: `Review ${req.params.id} deleted`,
+  })
+})
